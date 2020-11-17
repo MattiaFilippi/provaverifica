0a8dd9e7612d1997cfc0164f8fd513a492d304cc
MattiaFilippi <mattia.filippi.stud@ispascalcomandini.it>
2020-11-17 17:28:17 +0100

    Prova

---
+++ provaverifica/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace provaverifica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Wold!");
+        }
+    }
+}
